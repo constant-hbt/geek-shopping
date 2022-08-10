8e5eade06c657e3d9b3746537475ab7bbc770579
Henrique Borges Toninatto <henriqueborgesvotu@gmail.com>
2022-08-10 09:40:18 -0300

    :bricks: ci: Configurando AutoMapper e adicionando repositorio de Product.

--- GeekShopping.ProductApi/Program.cs
+++ GeekShopping.ProductApi/Program.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
+using GeekShopping.ProductApi.Config;
 using GeekShopping.ProductApi.Model.Context;
+using GeekShopping.ProductApi.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +17,12 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
---
+++ GeekShopping.ProductApi/Repository/ProductRepository.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using GeekShopping.ProductApi.Data.ValueObjects;
+using GeekShopping.ProductApi.Model;
+using GeekShopping.ProductApi.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductApi.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly DataContext _repository;
+        private IMapper _mapper;
+
+        public ProductRepository(DataContext repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductVO>> FindAll()
+        {
+            List<Product> products = await _repository.Products.ToListAsync();
+            return _mapper.Map<List<ProductVO>>(products);
+        }
+
+        public async Task<ProductVO> FindById(long id)
+        {
+            var product = await _repository.Products.FirstOrDefaultAsync(x => x.Id == id);
+            return _mapper.Map<ProductVO>(product);
+        }
+        public async Task<ProductVO> Create(ProductVO productVO)
+        {
+            var product = _mapper.Map<Product>(productVO);
+            await _repository.Products.AddAsync(product);
+            await _repository.SaveChangesAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+
+        public async Task<ProductVO> Update(ProductVO productVO)
+        {
+            var product = _mapper.Map<Product>(productVO);
+            await _repository.Products.AddAsync(product);
+            await _repository.SaveChangesAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+
+        public async Task<bool> Delete(long id)
+        {
+            try
+            {
+                var product = await _repository.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+                if (product == null) return false;
+                _repository.Products.Remove(product);
+                await _repository.SaveChangesAsync();
+                return true;
+            }catch(Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
